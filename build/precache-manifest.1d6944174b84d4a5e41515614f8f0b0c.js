fc075790324c82f9ef542fe0df558cfeda4cdeed
teemu <teemu.hallinen@hotmail.com>
2020-06-05 20:35:31 +0300

    frontend

---
+++ build/precache-manifest.1d6944174b84d4a5e41515614f8f0b0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2576ddaaf17fd958bd6aa862b960938",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7924c51d5aff7819cd49",
+    "url": "/static/css/main.5b90c400.chunk.css"
+  },
+  {
+    "revision": "801c77929d179622ee80",
+    "url": "/static/js/2.492a22ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.492a22ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7924c51d5aff7819cd49",
+    "url": "/static/js/main.f65db56d.chunk.js"
+  },
+  {
+    "revision": "2e46bddc6631f6e65431",
+    "url": "/static/js/runtime-main.1370e44a.js"
+  }
+]);
